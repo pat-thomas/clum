e30c13714a5b87b21f9b4607fb7460bd8ab8bdde
pat-thomas <patthomassoftware@gmail.com>
2017-01-25 11:04:19 -0500

    Fix test :require form

--- test/clum/core_test.clj
+++ test/clum/core_test.clj
@@ -1,7 +1,7 @@
 (ns clum.core-test
-  (:require [clojure.test :refer :all]
-            [clum.core :refer :all]))
+  (:require [org.httpkit.client :as http]
+            [clojure.test       :refer :all]))
 
-(deftest a-test
+(deftest api-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
