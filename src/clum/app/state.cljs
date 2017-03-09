04c6d9e48d38028ff1a7dafabfdcef732c396ffb
pat-thomas <patthomassoftware@gmail.com>
2017-03-09 09:43:02 -0500

    Whitespace.

--- src/clum/app/state.cljs
+++ src/clum/app/state.cljs
@@ -3,11 +3,11 @@
 
 (defonce app-state
   (let [initial-state (-> (reduce (fn [acc [x y]]
-                                  (assoc acc [x y] {:highlighted? false}))
-                                {}
-                                (for [x (range 8)
-                                      y (range 8)]
-                                  [x y]))
+                                    (assoc acc [x y] {:highlighted? false}))
+                                  {}
+                                  (for [x (range 8)
+                                        y (range 8)]
+                                    [x y]))
                           (merge {:higlighted-set #{}
                                   :connected?     false}))]
     (r/atom initial-state)))
